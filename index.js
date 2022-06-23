74b09a292c676db9357058fee6dfd35de94a6038
Nahuel Cacciatore <nahuelcacciatore@gmail.com>
2022-06-23 20:51:22 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/* for(let i = 0; i <= 5; i++){
+    if (i === 3){
+        continue;
+    }
+
+    console.log(i);
+}
+
+let nombre = prompt ("ingrese su nombre");
+let mensaje = `Hola ${nombre}`;
+
+alert (mensaje); */
+
+/* PARA CREAR LAS `` DEBO UTILIZAR ALT + 96 */
+
+for (let i = 0; i <= 5; i++) {
+    let nombre = prompt ("ingrese su nombre");
+    let mensaje = `hola ${nombre}`;
+
+    alert (mensaje);
+}
