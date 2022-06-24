e355433a6d90b1a946b407a8e9eb21b028322d61
Nahuel Cacciatore <nahuelcacciatore@gmail.com>
2022-06-23 21:15:35 -0300

    creando nuevo ciclo utilizando while

--- index.js
+++ index.js
@@ -13,9 +13,17 @@ alert (mensaje); */
 
 /* PARA CREAR LAS `` DEBO UTILIZAR ALT + 96 */
 
-for (let i = 0; i <= 5; i++) {
+/* for (let i = 0; i <= 5; i++) {
     let nombre = prompt ("ingrese su nombre");
     let mensaje = `hola ${nombre}`;
 
     alert (mensaje);
+} */
+
+let entrada = prompt ("Ingrese un usuario");
+
+while (entrada != "nahuel") {
+    alert ("Usuario invalido")
+
+    entrada = prompt ("Ingrese un usuario valido")
 }
