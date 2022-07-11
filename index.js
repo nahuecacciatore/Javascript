c4d489fe9ab878ecf5cff52265e050f0cff62055
unknown <nahuelcacciatore@gmail.com>
2022-07-10 22:02:59 -0300

    creando primera entrega

--- index.js
+++ index.js
@@ -26,34 +26,55 @@ alert (mensaje); */
 
 
 
-let precioInicial = 120000;
-
-function cuotas (precioInicial, cantidadCuotas) {
-    switch (cantidadCuotas) {
-        case 3:
-            return precioInicial / 3;
-            break;
-        case 6:
-            return precioInicial / 6;
-            break;
-        case 9:
-            return precioInicial / 9;
-            break;
-        case 12:
-            return precioInicial / 12;
-            break;
-        default:
-            return 0;
-            break;        
-    }
-}
-
-console.log(cuotas(precioInicial, 3));
 
 
-// ARRAYS //
-const cuotasDisponibles = [3,6,9,12];
-console.log( cuotasDisponibles.join (", "));
-// ARRAYS //
 
 
+
+//Estableciendo variables//
+let precioInicial = parseInt(
+    prompt("Por favor, ingrese el valor de su producto")
+  );
+  alert("El precio de su producto es " + precioInicial);
+  
+  let cantidadCuotas = parseInt(
+    prompt("Por favor indique cantidad de cuotas (3,6,9,12)")
+  );
+  let valorCuota = cuotas(precioInicial, cantidadCuotas);
+
+  alert(
+    "El valor de cada cuota sera de $" + valorCuota.toFixed(2)
+      
+    );
+  
+//Estableciendo funciones//
+  function cuotas(precioInicial, cantidadCuotas) {
+//Estableciendo ciclos//    
+    do {
+        switch (cantidadCuotas) {
+            case 3:
+              console.log("3= ", parseInt(precioInicial / 3));
+              return precioInicial / 3;
+              break;
+            case 6:
+              console.log("3= ", parseInt(precioInicial / 6));
+              return precioInicial / 6;
+              break;
+            case 9:
+              console.log("3= ", parseInt(precioInicial / 9));
+              return precioInicial / 9;
+              break;
+            case 12:
+              console.log("3= ", parseInt(precioInicial / 12));
+              return precioInicial / 12;
+              break;
+            default:
+              cantidadCuotas = parseInt (prompt("Ingrese una cantidad de cuotas validas"));    
+          }
+    } while (cantidadCuotas);
+  }
+
+
+// Arrays //
+    const cuotasDisponibles = [3,6,9,12];
+    console.log( cuotasDisponibles.join (", "));
